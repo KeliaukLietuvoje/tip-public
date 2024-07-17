10d6fd2fc79547c3de110a642288b64d07b27c53
Petras Pauliūnas <petras.pauliunas@gmail.com>
2024-07-17 13:41:09 +0300

    map fix

--- html/wpapp/themes/tip/inc/shortcodes/map/map.js
+++ html/wpapp/themes/tip/inc/shortcodes/map/map.js
@@ -6,21 +6,25 @@
             const mapContainer = document.getElementById('tip-map');
             if (!mapContainer || !maplibregl) return;
             const map = new maplibregl.Map({
-                container: 'sr-map',
+                container: 'tip-map',
                 style: 'https://basemap.startupgov.lt/vector/styles/bright/style.json',
                 center: tip_map_config.coordinates,
                 zoom: tip_map_config.zoom
             });
+            console.log(tip_map_config);
+            if (tip_map_config.add_layer === 'true') {
+                //add layer here
+            }else{
+                const el = document.createElement('div');
+                el.className = 'marker';
+                el.style.backgroundImage = `url(${tip_map_config.pin.url})`;
+                el.style.width = `${tip_map_config.pin.size[0]}px`;
+                el.style.height = `${tip_map_config.pin.size[1]}px`;
 
-            const el = document.createElement('div');
-            el.className = 'marker';
-            el.style.backgroundImage = `url(${tip_map_config.pin.url})`;
-            el.style.width = `${tip_map_config.pin.size[0]}px`;
-            el.style.height = `${tip_map_config.pin.size[1]}px`;
-
-            new maplibregl.Marker({element: el})
-                .setLngLat(tip_map_config.coordinates)
-                .addTo(map);
+                new maplibregl.Marker({element: el})
+                    .setLngLat(tip_map_config.coordinates)
+                    .addTo(map);
+            }
         }
     }
     document.addEventListener('DOMContentLoaded', () => {
--- html/wpapp/themes/tip/inc/shortcodes/map/map.php
+++ html/wpapp/themes/tip/inc/shortcodes/map/map.php
@@ -11,11 +11,12 @@ class TIP_Map
     {
         $atts = shortcode_atts(
             array(
-                'coordinates' => '23.7486, 55.0904',  // Lietuvos geografinė centro vieta
+                'coordinates' => '23.7486, 55.0904', 
                 'zoom' => '7',
-                'pin' => TIP_THEME_URL . '/assets/images/pin3.svg',
+                'pin' => TIP_THEME_URL . '/inc/shortcodes/map/pin3.svg',
                 'pin_size' => '26,38',
-                'map_height' => '800px'
+                'map_height' => '800px',
+                'add_layer'=> 'false'
             ),
             $atts,
             'tip_map'
@@ -24,8 +25,8 @@ class TIP_Map
         $maplibre_css_ver = $maplibre_js_ver = '4.3.2';
         wp_enqueue_style('maplibre-styles', '//unpkg.com/maplibre-gl@'.$maplibre_js_ver.'/dist/maplibre-gl.css', [], $maplibre_js_ver, 'all');
         wp_enqueue_script('maplibre-js', '//unpkg.com/maplibre-gl@'.$maplibre_css_ver.'/dist/maplibre-gl.js', ['jquery'], $maplibre_css_ver, true);
-        $tip_map_js_ver  = date("ymd-His", filemtime(SR_THEME_DIR . '/inc/shortcodes/map/map.js'));
-        $tip_map_css_ver  = date("ymd-His", filemtime(SR_THEME_DIR . '/inc/shortcodes/map/map.css'));
+        $tip_map_js_ver  = date("ymd-His", filemtime(TIP_THEME_DIR . '/inc/shortcodes/map/map.js'));
+        $tip_map_css_ver  = date("ymd-His", filemtime(TIP_THEME_DIR . '/inc/shortcodes/map/map.css'));
         wp_enqueue_style('tip-map-styles', TIP_THEME_URL . '/inc/shortcodes/map/map.css', [], $tip_map_css_ver, 'all');
         wp_enqueue_script('tip-map-js', TIP_THEME_URL . '/inc/shortcodes/map/map.js', ['jquery'], $tip_map_js_ver, true);
 
@@ -37,6 +38,7 @@ class TIP_Map
                 'url'=>$atts['pin'],
                 'size'=> explode(',',$atts['pin_size'])
             ],
+            'add_layer' => $atts['add_layer']
         ));
 
         return '<div class="tip-map__wrapper"><div id="tip-map" style="height:'.$atts['map_height'].'"></div></div>';
