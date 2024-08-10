e82957d31178debc90428a49fa8ce9da39d3310a
Petras Pauliūnas <petras.pauliunas@gmail.com>
2024-08-10 10:50:35 +0300

    plugins update, map add layer

--- html/wpapp/themes/tip/inc/shortcodes/map/map.js
+++ html/wpapp/themes/tip/inc/shortcodes/map/map.js
@@ -13,7 +13,54 @@
             });
             console.log(tip_map_config);
             if (tip_map_config.add_layer === 'true') {
-                //add layer here
+                map.on('load', () => {
+                    map.addSource('registras', {
+                        type: 'vector',
+                        tiles: [tip_map_config.api.url + '/tiles/objects/{z}/{x}/{y}'],
+                    });
+
+                    map.addLayer({
+                        id: 'cluster-circle',
+                        type: 'circle',
+                        filter: ['all', ['has', 'cluster_id']],
+                        paint: {
+                            'circle-color': '#003D2B',
+                            'circle-opacity': 0.3,
+                            'circle-radius': 20,
+                        },
+                        source: 'registras',
+                        'source-layer': 'objects',
+                    });
+
+                    map.addLayer({
+                        id: 'point',
+                        type: 'circle',
+                        source: 'registras',
+                        filter: ['all', ['!has', 'cluster_id']],
+                        paint: {
+                            'circle-color': '#003D2B',
+                            'circle-opacity': 1,
+                            'circle-radius': 5,
+                        },
+                        'source-layer': 'sportsBases',
+                    });
+
+                    map.addLayer({
+                        id: 'cluster',
+                        type: 'symbol',
+                        source: 'registras',
+                        'source-layer': 'sportsBases',
+                        filter: ['all', ['has', 'cluster_id']],
+                        layout: {
+                            'text-field': "{point_count}",
+                            'text-font': ['Noto Sans Regular'],
+                            'text-size': 16,
+                        },
+                        paint: {
+                            'text-color': '#000000'
+                        },
+                    });
+                });
             }else{
                 const el = document.createElement('div');
                 el.className = 'marker';
